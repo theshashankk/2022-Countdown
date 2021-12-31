c2fc0a6a879e2cf92df0b988fb6097065f5bb7b4
Shashank Sharma <78837537+theshashankk@users.noreply.github.com>
2021-12-31 10:52:37 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+NAME = os.environ.get("NAME", default="theshashankk")
+IMAGE = os.environ.get("IMAGE", default="https://telegra.ph/file/10ff6d59629bed12a47ef.jpg")
+SHORT_BIO = os.environ.get("SHORT_BIO", default="2022 Countdown")
+ABOUT = os.environ.get("ABOUT", default="it's just a countdown, ntg else ;)")
+GITHUB = os.environ.get("GITHUB", default=None)
+FACEBOOK = os.environ.get("FACEBOOK", default=None)
+INSTAGRAM = os.environ.get("INSTAGRAM", default=None)
