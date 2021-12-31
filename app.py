a4747881b7110a0aa3da094667ef0bca95e8b0bb
Shashank Sharma <78837537+theshashankk@users.noreply.github.com>
2021-12-31 10:48:59 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from heroku import NAME,IMAGE, SHORT_BIO, ABOUT, GITHUB, FACEBOOK, INSTAGRAM
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html", name=NAME, image=IMAGE, bio=SHORT_BIO, about=ABOUT, git=GITHUB, fb=FACEBOOK, ig=INSTAGRAM)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
